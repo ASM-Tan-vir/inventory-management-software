9fb33998725155f1b24ded53b7a550c5094ccd83
Tanvir <asmtanvir45@gmail.com>
2025-11-11 13:57:22 +0600

    header done

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { History } from 'lucide-react'
+import Search from './Search'
 
 export default function Header() {
   return (
-    <div>
-        <h2 className='bg-slate-300'>Header is Hare</h2>
+    <div className='bg-gray-100 h-14 flex items-center justify-between px-8'>
+        <div className='flex gap-3'>
+          <button> <History className='w-6 h-6'/>  </button>
+          <Search />
+        </div>
     </div>
   )
 }
